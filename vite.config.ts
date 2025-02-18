7cd3d4fa052477be89cfc539408476f6b8e66b57
BurnsN-Prog <ndburns222@gmail.com>
2025-02-18 13:05:05 -0600

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/AI-Market-Research/', // same as your repo name
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+    
+  
+  })
+
 
 
